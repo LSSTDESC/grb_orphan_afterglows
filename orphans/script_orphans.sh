19ef1846c045a60a3ba54cdab06861a93bd20bca
Marina Masson <marina.masson@lpsc.in2p3.fr>
2023-07-19 11:24:25 +0200

    bug fixed script

--- orphans/script_orphans.sh
+++ orphans/script_orphans.sh
@@ -10,7 +10,7 @@
 
 #SBATCH --ntasks=1                      # Exécuter une seule tâche
 #SBATCH --mem=2000                      # Mémoire en MB par défaut
-#SBATCH --time=0-01:00:00               # Délai max = 7 jours
+#SBATCH --time=0-00:15:00               # Délai max = 7 jours
 
 
 # Commandes à soumettre :
@@ -18,11 +18,11 @@
 ccenv anaconda
 conda activate /pbs/home/m/mmasson/lsst/orphans_env
 	
-export RUBIN_SIM_DATA = /pbs/home/m/mmasson/lsst/rubin_sim_data
-export RUBIN_SIM = /pbs/home/m/mmasson/lsst/rubin_sim
-export DUSTMAPS = /pbs/home/m/mmasson/lsst/orphans/data/schlafly_dust_factor.csv
-export SIMU = /pbs/home/m/mmasson/lsst/orphans/data/simulations
-export OBS = /pbs/home/m/mmasson/lsst/orphans/data/pseudo_obs
+export RUBIN_SIM_DATA=/pbs/home/m/mmasson/lsst/rubin_sim_data
+export RUBIN_SIM=/pbs/home/m/mmasson/lsst/rubin_sim
+export DUSTMAPS=/pbs/home/m/mmasson/lsst/orphans/data/schlafly_dust_factor.csv
+export SIMU=/pbs/home/m/mmasson/lsst/orphans/data/simulations
+export OBS=/pbs/home/m/mmasson/lsst/orphans/data/pseudo_obs
 	
-module load python
+#module load python
 python generate_grb_pop.py
