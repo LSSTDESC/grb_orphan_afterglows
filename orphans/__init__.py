b010265ca7fbe91615159569369751bd2229af35
Johan Bregeon <johan.bregeon@gmail.com>
2026-05-10 19:05:42 +0200

    add skysurvey as a submodule, add notebooks and data

--- orphans/__init__.py
+++ orphans/__init__.py
@@ -3,6 +3,7 @@
 # Export core symbols
 from .grb_interface import make_grb_light_curve, make_grb_spectrum, dump_wl_Fnu_spectrum
 from .tools import flux_to_mag, get_wl_and_nu_band
+from .skysurvey import load_data
 from .grb_configs import GRB_BASE_PARAMS
 from .jetsimpy_interface import make_jet_light_curve, make_jet_spectrum
 
