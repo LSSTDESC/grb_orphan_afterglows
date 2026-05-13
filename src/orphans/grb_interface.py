618d15a5b2660145bf5a220dd79b3cf1bb1ab2a3
Johan Bregeon <johan.bregeon@gmail.com>
2026-05-13 22:07:38 +0200

    pylint grb_interface.py

--- src/orphans/grb_interface.py
+++ src/orphans/grb_interface.py
@@ -3,17 +3,20 @@
 This module provides the main interface to afterglowpy
 '''
 
-import math
-import numpy as np
-import sys
+# Standard library imports
 from copy import deepcopy
+
+# Third‑party imports
+import numpy as np
 import afterglowpy as grb
 from astropy.cosmology import Planck18 as cosmo
 
-from orphans.tools import flux_to_mag, get_wl_and_nu_band
+# Local imports
+from orphans.tools import get_wl_and_nu_band
 from orphans.grb_configs import GRB_BASE_PARAMS
 
 
+# pylint: disable=invalid-name
 def make_grb_light_curve(E0=1.0e53, thetaObs=0.05, thetaCore=0.1, freq=5.0e14):
     """ Compute GRB light curve
 
@@ -25,7 +28,7 @@ def make_grb_light_curve(E0=1.0e53, thetaObs=0.05, thetaCore=0.1, freq=5.0e14):
     :return: arrays of frequency, time and fluxes in Jy
     """
     # for convenience, place arguments into a dict.
-    Z = deepcopy(GRB_BASE_PARAMS)
+    Z = deepcopy(GRB_BASE_PARAMS)  # pylint: disable=invalid-name
     Z['E0'] = E0
     Z['thetaObs'] = thetaObs
     Z['thetaCore'] = thetaCore
@@ -44,31 +47,41 @@ def make_grb_light_curve(E0=1.0e53, thetaObs=0.05, thetaCore=0.1, freq=5.0e14):
     return nu, t, Fnu_Jy
 
 
-def make_grb_spectrum(jetType=4, E0=1.0e53, z=1, n0=1., thetaObs=0.05, thetaCore=0.1, thetaWing=0.15, specType=0, t=1.0 * grb.day2sec):
+def make_grb_spectrum(
+    jetType=4,  # pylint: disable=invalid-name
+    E0=1.0e53,  # pylint: disable=invalid-name
+    z=1,
+    n0=1.,
+    thetaObs=0.05,  # pylint: disable=invalid-name
+    thetaCore=0.1,  # pylint: disable=invalid-name
+    thetaWing=0.15,  # pylint: disable=invalid-name
+    specType=0,  # pylint: disable=invalid-name
+    t=1.0 * grb.day2sec,
+):  # pylint: disable=too-many-arguments,too-many-positional-arguments
     """ Compute GRB SED
     1.0 * grb.day2sec is just 1 day
     """
     # For convenience, place arguments into a dict.
-    Z = deepcopy(GRB_BASE_PARAMS)
-    Z['jetType'] = jetType
-    Z['specType'] = specType
-    Z['E0'] = E0
+    Z = deepcopy(GRB_BASE_PARAMS)  # pylint: disable=invalid-name
+    Z['jetType'] = jetType  # pylint: disable=invalid-name
+    Z['specType'] = specType  # pylint: disable=invalid-name
+    Z['E0'] = E0  # pylint: disable=invalid-name
     Z['z'] = z
     Z['d_L'] = cosmo.luminosity_distance(Z['z']).value * 3.08e24
     Z['n0'] = n0
-    Z['thetaObs'] = thetaObs
-    Z['thetaCore'] = thetaCore
-    Z['thetaWing'] = thetaWing
+    Z['thetaObs'] = thetaObs  # pylint: disable=invalid-name
+    Z['thetaCore'] = thetaCore  # pylint: disable=invalid-name
+    Z['thetaWing'] = thetaWing  # pylint: disable=invalid-name
     # first create a wavelength range from 200 to 1300 nm
     wl_full_band, freq_full_band = get_wl_and_nu_band()
     # calculate but Fnu is in mJy by default
     fnu = grb.fluxDensity(t, freq_full_band, **Z)
     # so we convert to Jy
-    Fnu_Jy = fnu * 1.0e-3
+    Fnu_Jy = fnu * 1.0e-3  # pylint: disable=invalid-name
     return wl_full_band, freq_full_band, t, Fnu_Jy
 
 
-def dump_wl_Fnu_spectrum(wavelenghts, Fnu_Jy, file_name="grb_sed.txt"):
+def dump_wl_Fnu_spectrum(wavelenghts, Fnu_Jy, file_name="grb_sed.txt"):  # pylint: disable=invalid-name
     """ Get arrays for a given wavelength band
 
     Parameters
@@ -85,7 +98,7 @@ def dump_wl_Fnu_spectrum(wavelenghts, Fnu_Jy, file_name="grb_sed.txt"):
     0 : if file was properly written on disk
     """
     print(f"Writing {file_name}")
-    with open(file_name, 'w') as f:
+    with open(file_name, 'w', encoding='utf-8') as f:
         f.write("# lambda(nm)   Fnu(Jy)\n")
         for wl, fnu in zip(wavelenghts, Fnu_Jy):
             f.write(f'{wl:.1f}\t{fnu:.6f}\n')
